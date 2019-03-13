aa96ed60c312d08a5f3062789fc16abd8645838a
Raineer <delaritaraineer81@gmail.com>
2019-03-14 01:49:37 +0800

    create before| run migrations/seeds on test db

--- test/app-test.js
+++ test/app-test.js
@@ -1,3 +1,12 @@
-describe('CRUD stickers', () => {
+const knex =require('../db/knex');
 
+describe('CRUD stickers', () => {
+    before(() => {
+        //run migrations
+        knex.migrate.latest()
+        .then(() => {
+             // run seeds
+            return knex.seed.run();
+        })
+    });
 });
