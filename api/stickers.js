b7d5e1c82086e904d96a28cb5b3e36c2b3d9185c
Raineer <delaritaraineer81@gmail.com>
2019-03-14 18:26:32 +0800

    error 404: not found

--- api/stickers.js
+++ api/stickers.js
@@ -15,10 +15,14 @@ router.get('/', (req, res) => {
    });
 });
 
-router.get('/:id', isValidId, (req, res) => {
-    res.json({
-        message: 'Hello!'
-    })
+router.get('/:id', isValidId, (req, res, next) => {
+    queries.getOne(req.params.id).then(sticker => {
+        if (sticker) {
+            res.json(sticker);
+        } else {
+            next();
+        }
+    });
 });
 
 module.exports = router;
