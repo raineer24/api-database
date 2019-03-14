eea39437c6aab3286778a1c7202145beb19cbf90
Raineer <delaritaraineer81@gmail.com>
2019-03-15 05:18:46 +0800

    Add postgres DB to Heroku

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,9 @@ module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/dummynerstore',
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
   };
