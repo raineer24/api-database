0ee48157a74c9c54eb82d738838da634e222b997
Raineer <delaritaraineer81@gmail.com>
2019-03-14 23:50:10 +0800

    validate id| line 7 and line

--- api/stickers.js
+++ api/stickers.js
@@ -42,8 +42,8 @@ router.post('/', (req, res, next) => {
    }
 });
 
-router.put('/:id', (req, res, next) => {
-    
-})
+router.put('/:id', isValidId , (req, res, next) => {
+
+});
 
 module.exports = router;
