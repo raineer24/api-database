ab19595e9ba3365aa939ef954b067e35b50ee35e
Raineer <delaritaraineer81@gmail.com>
2019-03-14 17:33:36 +0800

    show one record basic get hello message

--- api/stickers.js
+++ api/stickers.js
@@ -4,10 +4,21 @@ const router = express.Router();
 
 const queries = require('../db/queries');
 
+function isValidId(req, res, next) {
+    if(!isNaN(req.params.id)) return next();
+    next(new Error('Invalid ID'));
+}
+
 router.get('/', (req, res) => {
    queries.getAll().then(stickers => {
        res.json(stickers);
    });
+});
+
+router.get('/:id', isValidId, (req, res) => {
+    res.json({
+        message: 'Hello!'
+    })
 });
 
 module.exports = router;
